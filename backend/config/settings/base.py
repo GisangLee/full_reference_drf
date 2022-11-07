b610ba3758fd722074499a51be4dc24aeaeb255a
GisangLee <masterkorea01@naver.com>
2022-11-07 14:10:49 +0900

    d

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "config.urls.api"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [PROJ_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
